9b5ff7af4107f7aa991bfa4493c83d3c97812d52
Igor Józefowicz <igorjozefowicz@gmail.com>
2024-04-12 10:22:37 +0200

    update lab3 program

--- lab3/avp.py
+++ lab3/avp.py
@@ -1,12 +1,10 @@
 from exceptions import GameplayException
 from connect4 import Connect4
-from randomagent import RandomAgent
-from minmaxagent import MinMaxAgent
 from alphabetaagent import AlphaBetaAgent
 
 connect4 = Connect4(width=7, height=6)
 
-agent = MinMaxAgent('o')
+agent = AlphaBetaAgent('o')
 # agent = AlphaBetaAgent(my_token='o', depth=4)
 
 while not connect4.game_over:
